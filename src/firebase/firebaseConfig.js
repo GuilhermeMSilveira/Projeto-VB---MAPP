779259c4244310d16cae908554b4175e09a5926e
Guilherme Marques Silveira <128319947+GuilhermeMSilveira@users.noreply.github.com>
2025-04-06 08:30:35 -0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Sua configuração do Firebase
 const firebaseConfig = {
-    apiKey: 'AIzaSyA7W3xmnKjnnBJ7LBFiiHEM-jItJZIPDWk',
-    authDomain: 'testeuser@gmail.com',
-    projectId: 'guilhermem-29d59',
-    storageBucket: 'Teste.projetoTCC',
-    messagingSenderId: '1033451552965',
-    appId: '1:1033451552965:android:db6ae67c3953668507b0ac',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
 };
 
 // Inicializando o Firebase
