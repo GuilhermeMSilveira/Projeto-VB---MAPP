824ed8b5228c1fd544777ae6d2347899af376232
Guilherme Marques Silveira <128319947+GuilhermeMSilveira@users.noreply.github.com>
2025-05-18 10:25:48 -0300

    Update ConfiguracaoFirebase.js

--- src/services/ConfiguracaoFirebase.js
+++ src/services/ConfiguracaoFirebase.js
@@ -3,12 +3,12 @@ import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 // Sua configuração do Firebase
 const ConfiguracaoFirebase = {
-    apiKey: "AIzaSyCPTPhEY56QZ4A2hWKqa9EyMM_QAVuwaVM",
-    authDomain: "projetotcc8-5c2f9.firebaseapp.com",
-    projectId: "projetotcc8-5c2f9",
-    storageBucket: "projetotcc8-5c2f9.firebasestorage.app",
-    messagingSenderId: "984709933815",
-    appId: "1:984709933815:web:f4b0755f41e7c5022601f2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(ConfiguracaoFirebase);
